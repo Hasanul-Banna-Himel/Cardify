292c5a05dde95254f6da09f09eda0457521b2841
Mehreen Mallick <mehreen.mallick.fiona@g.bracu.ac.bd>
2024-08-20 12:51:29 +0600

    fixed clerk issue

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,10 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css';
 
+const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Cardify - The Ultimate Flashcard App',
   description: 'The best app to create, manage, and study flashcards effectively.',
@@ -9,10 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="bg-base-100 text-base-content">
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
