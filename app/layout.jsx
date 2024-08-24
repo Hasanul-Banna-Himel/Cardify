f85f5c694f1c7760bc7e0eaa2b5ea23579743cab
Hasanul Banna Himel <84923500+Hasanul-Banna-Himel@users.noreply.github.com>
2024-08-24 12:12:36 +0600

    m

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,12 +10,3 @@ export const metadata = {
   description: 'The best app to create, manage, and study flashcards effectively.',
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
-  );
-}
